1a1e0f33be1b647fa3d685128404645221c2f07f
venu67776 <83317608+venu67776@users.noreply.github.com>
2024-06-24 14:04:14 +0530

    day4

---
+++ Day4/functions.py
@@ -0,0 +1,15 @@
+def test(a, b):
+    add = a + b
+    return add
+test(4,5)
+
+def venu(a, b):
+    add = a - b
+    return abs(add)
+venu(6,7)
+
+def hi(name):
+    greet = f"hello,{name}"
+    return greet
+hi("venu")
+   
---
+++ Day4/newfunction.py
@@ -0,0 +1,2 @@
+import functions as test
+test
