57473a76a90d3f556b5c0a383eed503357ae3873
venu67776 <83317608+venu67776@users.noreply.github.com>
2024-06-26 11:53:28 +0530

    day7

---
+++ day7/condition _handling.py
@@ -0,0 +1,10 @@
+import sys # for using arguments sys.argv[]
+
+type == "sys.argv[1]"
+
+if type == "t2.micro": 
+    print(" the instance is t2.micro and it will charge you 2 dollars")
+elif type == "t2.medium":
+    print(" the instance is t2.medium and it will charge you 4 dollars")
+else:
+    print("provide valid instance type")
